7cbc8136380a8985bf20664dea8f4d82c33102c2
HanFuyong <fuyong.han@qq.com>
2022-10-16 23:34:42 +0800

    refactor folders

--- feature_tracking/feature_tracking_node.cpp
+++ feature_tracking/feature_tracking_node.cpp
@@ -9,7 +9,7 @@ using namespace Naive_SLAM_ROS;
 
 int main(int argc, char** argv){
     rclcpp::init(argc, argv);
-    std::string sParamPath = "/home/parallels/workspace/ros_ws/src/naive_slam_ros2/include/config/config.yaml";
+    std::string sParamPath = "/home/parallels/workspace/naive_slam_ros2_ws/src/config/config.yaml";
     rclcpp::spin(std::make_shared<FeatureTrackingNode>(sParamPath));
     rclcpp::shutdown();
     return 0;
